fb7b230ee4a52602873742fa0dbf1e0530dfa9a0
Artyom Tarynin <artem_tarynin@epam.com>
2018-07-28 12:27:36 +0300

    add pom

--- src/main/java/com/epam/lab/coding/client/Coding.java
+++ src/main/java/com/epam/lab/coding/client/Coding.java
@@ -1,5 +1,11 @@
 package com.epam.lab.coding.client;
 
-public class Coding {
+import com.google.gwt.core.client.EntryPoint;
 
+public class Coding implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+
+    }
 }
